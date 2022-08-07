68a8fee9b10b9d1352a7846be9cf88259c0e86bf
Aakash-bhandari <102407592+Aakash-bhandari@users.noreply.github.com>
2022-08-07 15:53:03 +0530

    DailyQuotes

---
+++ index.js
@@ -0,0 +1,26 @@
+let readabledata = '';
+const quotes = document.getElementById('text');
+const author = document.getElementById('author');
+const next = document.getElementById('next');
+const getnewquotes = () => {
+  let rnum = Math.floor(Math.random() * 1000);
+  quotes.innerHTML = `${readabledata[rnum].text}`
+  author.innerText = `${readabledata[rnum].author}`
+
+}
+
+const getquotes = async () => {
+  const api = "https://type.fit/api/quotes";
+  try {
+    const data = await fetch(api);
+    readabledata = await data.json();
+    getnewquotes();
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+next.addEventListener('click', () => {
+  getnewquotes();
+})
+getquotes();
